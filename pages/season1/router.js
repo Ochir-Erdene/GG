cd731ca4cbff9f82d12ab50d4f6b0848d8326113
tsolmonbat <tsolmon.bat2007@gmail.com>
2025-04-03 14:50:18 +0800

    fuck you

--- pages/season1/router.js
+++ pages/season1/router.js
@@ -46,7 +46,7 @@ export default function Home() {
           animate="animate"
           className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
         >
-          {["cv", "weather", "filter", "grid", "bieDaalt"].map((task) => (
+          {["Cv", "weather", "filter", "grid", "bieDaalt"].map((task) => (
             <motion.button 
               key={task}
               onClick={() => navigateTo(`/season1/${task}`)} 
